7992d86b0e2837056249f3fd856ef5441baf9a13
jaissymander <jsingh837@myseneca.ca>
2022-10-08 15:06:13 -0400

    bug fixes

--- A1/moviesAPI/server.js
+++ A1/moviesAPI/server.js
@@ -47,7 +47,7 @@ app.get("/api/movies", async (req, res) => {
       req.query.title || null
     );
     if (data.length === 0) {
-      return res.status(400).json(data);
+      return res.status(400).json([]);
     }
     res.json(data);
   } catch (error) {
